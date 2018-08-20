f075ce85e9093afbf4ff027b85268d037195cb5d
Victor Magalhaes Sales <victor.sales@MVFOR.local>
2018-08-20 16:47:23 -0300

    2

--- Rotas/src/app/app.module.ts
+++ Rotas/src/app/app.module.ts
@@ -1,13 +1,10 @@
+import { CursoModule } from './cursos/cursos.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
-import { CursosComponent } from './cursos/cursos.component';
-import { CursoDetalheComponent } from './cursos/curso-detalhe/curso-detalhe.component';
-import { CursosService } from './cursos/cursos.service';
-import { CursoNaoEncontradoComponent } from './cursos/curso-nao-encontrado/curso-nao-encontrado.component';
 import { AppRoutingModule } from './app.routing.module';
 
 @NgModule({
@@ -15,15 +12,13 @@ import { AppRoutingModule } from './app.routing.module';
     AppComponent,
     HomeComponent,
     LoginComponent,
-    CursosComponent,
-    CursoDetalheComponent,
-    CursoNaoEncontradoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CursoModule
   ],
-  providers: [CursosService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ Rotas/src/app/cursos/cursos.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CursosComponent } from './cursos.component';
+import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
+import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-encontrado.component';
+import { CursosService } from './cursos.service';
+import { AppRoutingModule } from './../app.routing.module';
+
+
+@NgModule({
+    imports: [
+        CommonModule,
+        AppRoutingModule
+    ],
+    exports: [],
+    declarations: [
+        CursosComponent,
+        CursoDetalheComponent,
+        CursoNaoEncontradoComponent
+    ],
+    providers: [CursosService]
+})
+export class CursoModule { }
